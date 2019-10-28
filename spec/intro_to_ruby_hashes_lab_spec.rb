977e046d29dcddb2f826c21fec87f298c9099212
Ingrid sura <sura.ingrid@gmail.com>
2019-10-28 18:26:11 +0000

    Automatically backed up by Learn

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -1,10 +1,10 @@
 require_relative 'spec_helper'
 require_relative '../intro_to_ruby_hashes_lab.rb'
 new_hash={}
-My_hash{id=4}
+My_hash{:id=>4}
   pioneer{:name=> "Grace Hopper"}
-  my_hash_creator{bad:2}
-  read_from_hash
+  my_hash_creator{:bad=>2}
+  read_from_hash{
 describe "working with hashes" do
 
   describe "instantiating" do
